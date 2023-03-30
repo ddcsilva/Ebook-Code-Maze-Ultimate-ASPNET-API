b274fe0747125cbf51aced47068fb685564433dc
danilo.silva <danilo.silva@msn.com>
2023-03-30 07:49:40 -0300

    3.3 - Classe de contexto e a conexão com o banco de dados

---
+++ FuncionariosEmpresa/FuncionariosEmpresa.API/RepositoryContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using FuncionariosEmpresa.Repository;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosEmpresa.API;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
---
+++ FuncionariosEmpresa/FuncionariosEmpresa.Repository/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using FuncionariosEmpresa.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuncionariosEmpresa.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Empresa>? Empresas { get; set; }
+    public DbSet<Funcionario>? Funcionarios { get; set; }
+}
