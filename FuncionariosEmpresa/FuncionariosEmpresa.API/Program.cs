079f55c6d132e61b3a500563a9c4f3b14acd41b1
danilo.silva <danilo.silva@msn.com>
2023-04-01 12:15:51 -0300

    4.1 - Controladores e Roteamento na API WEB

--- FuncionariosEmpresa/FuncionariosEmpresa.API/Program.cs
+++ FuncionariosEmpresa/FuncionariosEmpresa.API/Program.cs
@@ -13,7 +13,7 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddApplicationPart(typeof(FuncionariosEmpresa.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
---
+++ FuncionariosEmpresa/FuncionariosEmpresa.Presentation/Controllers/EmpresasController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FuncionariosEmpresa.Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmpresasController : ControllerBase
+{
+}
